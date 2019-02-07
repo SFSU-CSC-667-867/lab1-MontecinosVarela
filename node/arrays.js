06db1161b072b01e368f477585375f4642811855
MontecinosVarela <ivarela@mail.sfsu.edu>
2019-02-06 20:12:57 -0800

    finished variables.js

--- node/arrays.js
+++ node/arrays.js
@@ -15,15 +15,32 @@ array[1] = 'hello';
 // for practice
 
 // use forEach
+array.forEach(function(item, index, array) {
+    console.log(item, index);
+});
 
 // use map
+array[1] = 3;
+const map = array.map(x => x * 2);
+console.log(map);
 
 // use pop
+console.log('Array before being popped: ' + array);
+array.pop();
+console.log('Array after being popped: ' + array);
 
 // use push
+array.push('justPushed');
+console.log(array);
 
 // use shift
+array.shift();
+console.log('Shifted: ' + array);
 
 // use unshift
+array.unshift(9);
+console.log(array);
 
 // use filter
+const result = array.filter(value => value > 5);
+console.log(result);
--- node/classes.js
+++ node/classes.js
@@ -16,7 +16,25 @@ s.hello();
 // for practice
 
 // Create a subclass
+class SomethingElse extends Something {
+  constructor(name) {
+    super(name);
+    this.message = "how you doing?";
+  }
+
+  greetAndAsk() {
+    console.log(this.name + ' ' + this.message);
+  }
+
+  static staticMethod() {
+    return 'Im Static';
+  }
+}
 
 // add a property after the object has been instantiated
-
 // add a static function
+
+const somethingElse = new SomethingElse('tim')
+somethingElse.greetAndAsk();
+console.log(SomethingElse.staticMethod());
+
