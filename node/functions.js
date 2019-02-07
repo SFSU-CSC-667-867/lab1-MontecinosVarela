2ad4b13f8555e2b739aee0219ff9944d4473237e
MontecinosVarela <ivarela@mail.sfsu.edu>
2019-02-06 20:28:01 -0800

    functions.js done

--- node/functions.js
+++ node/functions.js
@@ -10,10 +10,17 @@ const arrow = (test) => {
   console.log(test);
 }
 
-arrow('hi')
+arrow('hi');
 
 // for practice
 
 // make an arrow function with no block body
+const myArrow = (y) => y + 1;
+console.log(myArrow(8));
 
 // make a function that returns a function
+const functionFactory = () => (n) => console.log('This is weird');
+const a = functionFactory();
+a();
+const anotherFunction = functionFactory;
+anotherFunction()(); 
