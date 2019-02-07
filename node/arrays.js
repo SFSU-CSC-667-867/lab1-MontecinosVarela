fe2829e066163a2004ae0b103576a19be732fc69
MontecinosVarela <ivarela@mail.sfsu.edu>
2019-02-06 20:37:40 -0800

    arrays.js done

--- node/arrays.js
+++ node/arrays.js
@@ -15,31 +15,31 @@ array[1] = 'hello';
 // for practice
 
 // use forEach
-array.forEach(function(item, index, array) {
-    console.log(item, index);
+const arrayTest = [9, 3, 4];
+console.log(arrayTest);
+arrayTest.forEach((n) => {
+    console.log(n);
 });
 
 // use map
-array[1] = 3;
-const map = array.map(x => x * 2);
-console.log(map);
+const newArray = arrayTest.map(i => i + 1);
+console.log(newArray);
 
 // use pop
-console.log('Array before being popped: ' + array);
-array.pop();
-console.log('Array after being popped: ' + array);
+newArray.pop();
+console.log(newArray);
 
 // use push
-array.push('justPushed');
-console.log(array);
+newArray.push(6);
+console.log(newArray);
 
 // use shift
-array.shift();
-console.log('Shifted: ' + array);
+newArray.shift();
+console.log(newArray);
 
 // use unshift
-array.unshift(9);
-console.log(array);
+newArray.unshift(5);
+console.log(newArray);
 
 // use filter
 const result = array.filter(value => value > 5);
